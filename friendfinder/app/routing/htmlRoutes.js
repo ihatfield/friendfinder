5ed4ac61180d8879c3ef33ebbe920f5dbd09d54e
Isla Hatfield <30755706+ihatfield@users.noreply.github.com>
2018-02-26 20:03:22 -0700

    Delete htmlRoutes.js

--- friendfinder/app/routing/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const questions = require('./../data/questions.js')
-
-router.get('/', (req, res) => res.render('home'));
-
-router.get('/survey', (req, res) => res.render('survey', {questions,}))
-
-module.exports = router;
