7cf5b898a6a1574c7daeafa9b36a900cdbe5aa4c
Isla Hatfield <30755706+ihatfield@users.noreply.github.com>
2018-02-26 23:50:03 -0600

    added

---
+++ friendfinder/app/data/friends.js
@@ -0,0 +1,137 @@
+var friends = [
+	{
+	 "name":"Collin",
+  	"photo":"",
+  	"scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+
+	},
+
+  {
+    "name":"Tom",
+    "photo":"https://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+
+  },
+
+
+  {
+    "name":"Nick",
+    "photo":"https://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+
+  },
+
+
+  {
+    "name":"Karen",
+    "photo":"https://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      2,
+      3,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      2,
+      3
+    ]
+
+  },
+
+
+  {
+    "name":"Mei",
+    "photo":"https://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      2,
+      3,
+      4,
+      4,
+      5,
+      4,
+      1,
+      1,
+      1,
+      1
+    ]
+
+  },
+
+
+  {
+    "name":"Alyssa",
+    "photo":"hhttps://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      5,
+      2,
+      3,
+      1,
+      5,
+      1,
+      2,
+      4,
+      2,
+      1
+    ]
+
+  },
+
+  {
+  
+    "name":"Roxy",
+    "photo":"https://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
+    "scores":[
+      5,
+      5,
+      4,
+      4,
+      5,
+      3,
+      3,
+      5,
+      4,
+      3
+    ]
+
+  }
+
+
+
+]; //closing friends
+
+module.exports = friends;
